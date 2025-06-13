de0f347392bf54bfd70262cf47ced601588c2c9a
Renan G. Klettenberg <128403861+Penisdrivoso@users.noreply.github.com>
2025-06-13 18:14:07 -0300

    Mudança posição arquivos css

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\VendaController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AvisoController;
+use App\Http\Controllers\LoginController;
 
 use Illuminate\Support\Facades\Route;
 
 Route::resource('vendas',VendaController::class);
 Route::resource('produtos',ProdutoController::class);
 Route::resource('user',UserController::class);
+Route::resource('login',LoginController::class);
 Route::resource('caixas',ProdutoController::class);
 Route::resource('avisos',AvisoController::class);
